e718422d79a20409829fbc5fa682b7bb67e40474
wang <beiluonever@qq.com>
2020-11-30 22:18:56 +0800

    init commit

---
+++ service-b/src/main/java/com/example/serviceb/IndexController.java
@@ -0,0 +1,22 @@
+package com.example.serviceb;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * gateway-test
+ * test index
+ * </p>
+ *
+ * @author beiluo1994@gmail.com
+ * @since 2020-11-30 21:27
+ **/
+@RestController
+public class IndexController {
+
+    @GetMapping("serviceb")
+    public String serviceA(){
+        return "this is service B";
+    }
+}
